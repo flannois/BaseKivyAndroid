e8b73565dbc29dd85f5bd459028d742ca39f6d55
flannois <florent.lannois@gmail.com>
2020-04-20 17:05:03 +0200

    Premier Menu

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
-class TestAndroid(BoxLayout):
-    def __init__(self,**kwargs):
-        super(TestAndroid,self).__init__(**kwargs)
+import sys
 
+class Fenetre(BoxLayout):
 
+        container = ObjectProperty(None)
 
-class TestAndroidApp(App):
+class AppliApp(App):
     def build(self):
-        Builder.load_file('fichier.kv')
-        return TestAndroid()
+        self.root = Builder.load_file('kv/menu.kv')
+    
+    def menu(self,fenetreActuelle):
+        self.changementFenetre(fenetreActuelle,'menu.kv')
 
+    def jouer(self,fenetreActuelle):
+        self.changementFenetre(fenetreActuelle,'jouer.kv')
 
+    def option(self,fenetreActuelle):
+        self.changementFenetre(fenetreActuelle,'option.kv')
+    
+    def changementFenetre(self,fenetreActuelle,fenetreSuivante):
+        Builder.unload_file('kv/'+fenetreActuelle)
+        self.root.container.clear_widgets()
+        screen = Builder.load_file('kv/'+fenetreSuivante)
+        self.root.container.add_widget(screen)
 
+    def quitter(self):
+        sys.exit()
 
 if __name__ == "__main__":
     
-    TestAndroidApp().run()
+    AppliApp().run()
