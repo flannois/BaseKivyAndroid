1ca9cbdcbb8d64e737f0cf182a5a6bb22d9fd5a4
flannois <florent.lannois@gmail.com>
2020-12-15 21:37:17 +0100

    Interface OK

--- main.py
+++ main.py
@@ -1,36 +1,22 @@
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
+from kivy.config import Config
+Config.set('graphics', 'width',  720)
+Config.set('graphics', 'height', 720)
 
-import sys
+        
 
-class Fenetre(BoxLayout):
-
-        container = ObjectProperty(None)
-
-class AppliApp(App):
+class ScrabbleApp(App):
     def build(self):
-        self.root = Builder.load_file('kv/menu.kv')
-    
-    def menu(self,fenetreActuelle):
-        self.changementFenetre(fenetreActuelle,'menu.kv')
+        pass
 
-    def jouer(self,fenetreActuelle):
-        self.changementFenetre(fenetreActuelle,'jouer.kv')
 
-    def option(self,fenetreActuelle):
-        self.changementFenetre(fenetreActuelle,'option.kv')
-    
-    def changementFenetre(self,fenetreActuelle,fenetreSuivante):
-        Builder.unload_file('kv/'+fenetreActuelle)
-        self.root.container.clear_widgets()
-        screen = Builder.load_file('kv/'+fenetreSuivante)
-        self.root.container.add_widget(screen)
-
-    def quitter(self):
-        sys.exit()
 
 if __name__ == "__main__":
     
-    AppliApp().run()
+    ScrabbleApp().run()
+
+
+
+
+
+
